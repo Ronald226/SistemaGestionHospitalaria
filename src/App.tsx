c1bcefa558320c6c33416338242bbfb48d0ebf32
Ronald H <rhuamanipaco@unsa.edu.pe>
2025-06-30 04:25:18 -0500

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-import AppRoutes from './routes/AppRoutes'
+import './App.css';
+import AppRoutes from './routes/AppRoutes';
+import { useEffect } from 'react';
+import '@n8n/chat/style.css';
+import { createChat } from '@n8n/chat';
+
 function App() {
+  useEffect(() => {
+    createChat({
+      webhookUrl: 'https://ron22hp.app.n8n.cloud/webhook/f54cb8ca-4510-4727-8142-45bc0a2906d9/chat', 
+      mode: 'window', 
+      defaultLanguage: 'es',
+      initialMessages: [
+        '👋 ¡Hola! ¿Deseas reservar una cita médica?',
+      ],
+      i18n: {
+        es: {
+          title: 'Asistente del Centro Médico Pedro P. Diaz',
+          subtitle: 'Chatea con nosotros para agendar tu cita.',
+          getStarted: 'Nueva conversación',
+          inputPlaceholder: 'Escribe aquí tu mensaje...',
+        }
+      }
+    });
+  }, []); 
+
   return (
     <>
-      <AppRoutes></AppRoutes>
+      <AppRoutes />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
